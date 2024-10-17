82d8d24696941c80bcd51d0ad27cc8410109d92b
Francisco Valdes <ecodisonante@gmail.com>
2024-10-16 23:33:04 -0300

    initial commit

---
+++ src/test/java/com/ecodisonante/booktopia/BooktopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecodisonante.booktopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
